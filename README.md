f7818cc09a4fcee734bb8950a1a056955cc6166a
Joey574 <joeysoroka@outlook.com>
2026-02-02 13:54:30 -0800

    stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+# DSA
+This is a test repo for me to make data structures and algorithms in whatever language I feel like, so when I finally take DSA I can just lay down and rest :)
---
+++ Set/cpp/set.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+template <typename T>
+struct Set {
+    public:
+
+    private:
+    size_t compute_idx_(const T& v) {
+        return 0;
+    }
+    T* data_;
+};
